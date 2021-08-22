5a15c60469ad18b752024fb38bf7f8a24caa5749
Gaurav Singh <gauravsingh.info@gmail.com>
2021-08-22 20:05:29 +0530

    created react basic cra for development

---
+++ node_task/demo.js
@@ -0,0 +1,45 @@
+// // const somefun = () => {
+
+// //     let d;
+    
+// //     if (d > 5) {
+// //         d = 6;
+    
+// //         console.log(d)
+// //     }
+    
+// //     if (!d) {
+// //        console.log(d)
+// //     }}
+    
+
+// // console.log("first");
+// // setTimeout(() => {
+// //     console.log("second");
+// // }, 0);
+// // console.log(“third”);
+
+// const person = {
+//     age: 20,
+//     name: 'Ben'
+//   };
+  
+//   const peopleArray = [person, person, person];
+
+//   peopleArray[1].name = 'Joe';
+
+//   let persons;
+
+//   if (!persons) {
+//    peopleArray[2].name='amigo';
+//   }
+
+//   console.log(peopleArray[0].name);
+  
+// const StudentModel = require(../)
+
+// app.post('/data', async(req, res) => {
+//     // const student = await StudentModel.
+//     await StudentModel.group('Name')
+// })
+
